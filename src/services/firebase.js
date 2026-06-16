f8df13db1907b1f8c2eba7784f4d979bf8a9d4ae
luciapons <luciaponss@hotmail.com>
2026-06-16 17:34:04 -0300

    migracion de config firebase a variables de entorno y restriccion de api key en google cloud

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,17 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBXzTnYSb5OlTvB-Bu4w0ix-DWd3Q8bkgs",
-  authDomain: "proyectoreact-1468a.firebaseapp.com",
-  projectId: "proyectoreact-1468a",
-  storageBucket: "proyectoreact-1468a.firebasestorage.app",
-  messagingSenderId: "779237748",
-  appId: "1:779237748:web:cc0bb1fa2843f61ada1421"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
